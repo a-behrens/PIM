c25a9ffe97296c77a69ac89c00f13f9d5c70d277
Alejandro Behrens <a.a.behrens@student.vu.nl>
2019-06-12 11:18:46 +0200

    Major changes

---
+++ public/js/animate.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    console.log("jquery ready"),
+    setTimeout(function(){
+        $('.trans--grow').addClass('grow');
+    }, 1000);
+});
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+
+})
+
+
+
+
